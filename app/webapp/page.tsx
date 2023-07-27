a5744dcc2e80a4cf2e49fbd4dc368cce425e64e4
Issack John <johnissack997@gmail.com>
2023-07-27 13:16:03 -0700

    center iframe

--- app/webapp/page.tsx
+++ app/webapp/page.tsx
@@ -1,6 +1,6 @@
 export default function Home() {
   return (
-    <div>
+    <div className="flex min-h-screen flex-col items-center justify-between p-24">
       <iframe
         width={600}
         height={600}
