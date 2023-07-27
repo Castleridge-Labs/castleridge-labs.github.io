cc42afb888053fd815c030f46ecc76a73e400a77
Issack John <johnissack997@gmail.com>
2023-07-27 13:15:14 -0700

    add webapp

---
+++ app/webapp/page.tsx
@@ -0,0 +1,12 @@
+export default function Home() {
+  return (
+    <div>
+      <iframe
+        width={600}
+        height={600}
+        title="ufc-calendar-chrome-extension"
+        src="https://main.dge8uvx5u74zp.amplifyapp.com/"
+      ></iframe>
+    </div>
+  );
+}
