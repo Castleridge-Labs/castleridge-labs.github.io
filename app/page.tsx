a164287e26dd95ab444ca5b19e750ce5d5f60c2e
Issack John <johnissack997@gmail.com>
2023-07-27 20:20:36 -0700

    reduce size and bug fix

--- app/page.tsx
+++ app/page.tsx
@@ -20,7 +20,7 @@ export default function Home() {
       </div>
 
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <div className="fixed left-0 top-0 flex w-full justify-center lg:static lg:w-auto">
+        <div className="left-0 top-0 flex w-full justify-center lg:static lg:w-auto">
           <a
             href="https://chrome.google.com/webstore/detail/ufc-calendar-with-live-up/pdagojbjdekpdicaefbiolkpgobiaoch"
             className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
@@ -31,7 +31,6 @@ export default function Home() {
               src={"/chromestore.png"}
               alt="logo"
               width={206}
-              style={{ maxWidth: "100%" }}
               height={56}
             />
             <h2 className={`mb-3 text-2xl font-semibold`}>
