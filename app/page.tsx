9b1629c5e7b9372c9508787008750f1114d4b2c0
Issack John <johnissack997@gmail.com>
2023-08-02 18:02:44 -0700

    remove some styling

--- app/page.tsx
+++ app/page.tsx
@@ -3,14 +3,14 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className=" w-full relative flex justify-around place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <p className="max-w-[25ch] text-2xl font-semibold">
+      <div className="w-full md:flex md:justify-around place-items-center z-[-1]">
+        <p className="w-full md:max-w-[25ch] md:text-2xl font-semibold mb-10">
           Conveniently get live updates of UFC events and view previous/upcoming
           events with one click.
         </p>
 
         <Image
-          className="relative"
+          className="min-w-[30ch]"
           src="/one.png"
           alt="ufc calendar picture"
           width={400}
