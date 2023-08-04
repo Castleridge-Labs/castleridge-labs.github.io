b6d36f7df01863e9fc72c920414fb62641a8902f
Issack John <johnissack997@gmail.com>
2023-08-03 17:57:55 -0700

    fixed sizing issue

--- app/extension-survey/page.tsx
+++ app/extension-survey/page.tsx
@@ -1,16 +1,15 @@
 "use client";
-import { Widget } from '@typeform/embed-react'
+import { Widget } from "@typeform/embed-react";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col sm:items-center justify-between p-10">
-      <div className="">
-      <Widget
-        id="qExZynKI"
-        className="my-form h-screen w-screen"
-        iframeProps={{ title: "UFC Calendar Feedback" }}
-      />
-      </div>
+    <main className="flex flex-col px-24 h-screen">
+        <Widget
+          id="qExZynKI"
+          className="my-form h-4/6 w-full"
+          iframeProps={{ title: "UFC Calendar Feedback" }}
+        />
+
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
         <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
           <a
