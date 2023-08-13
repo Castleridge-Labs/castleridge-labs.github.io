af148853495f80b2771ad8433ef57e9dcc6e6491
Issack John <johnissack997@gmail.com>
2023-08-13 13:19:15 -0700

    looks better on iphone

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -11,7 +11,7 @@ export default function Home() {
       return compareDesc(new Date(a.date), new Date(b.date));
     });
   return (
-    <main className="container flex min-h-screen flex-col items-center justify-between p-24 max-w-4xl py-6 lg:py-10">
+    <main className="container flex min-h-screen flex-col items-center justify-between sm:p-24 max-w-4xl py-6 lg:py-10">
       <div className="flex flex-col items-start gap-4 md:flex-row md:justify-between md:gap-8">
         <div className="flex-1 space-y-4">
           <h1 className="inline-block font-heading text-4xl tracking-tight lg:text-5xl">
--- app/page.tsx
+++ app/page.tsx
@@ -2,15 +2,15 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-8 md:p-24">
+    <main className="flex min-h-screen flex-col items-center sm:justify-between container md:p-28">
       <div className="w-full md:flex md:justify-around place-items-center z-[-1]">
-        <p className="text-balance w-full md:max-w-[30ch] text-xl lg:text-3xl font-semibold mb-10">
+        <p className="text-balance w-full md:max-w-[30ch] text-xl lg:text-3xl font-semibold mb-1 mt-1">
           Get live updates of UFC events and view previous/upcoming events with
           one click.
         </p>
 
         <Image
-          className="min-w-[30ch] rounded-md border bg-muted transition-colors m"
+          className="min-w-[30ch] rounded-md border bg-muted transition-colors"
           src="/one.png"
           alt="ufc calendar picture"
           width={400}
@@ -19,28 +19,24 @@ export default function Home() {
         />
       </div>
 
+      <div className="flex flex-row w-full lg:ml-80 mt-10 sm:mt-0">
+        <a
+          href="https://chrome.google.com/webstore/detail/ufc-calendar-with-live-up/pdagojbjdekpdicaefbiolkpgobiaoch"
+          className="group rounded-lg border px-5 py-4 transition-colors border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <Image src={"/chromestore.png"} alt="logo" width={340} height={96} />
+          <h2 className={`mb-3 text-2xl font-semibold`}>
+            Get UFC Calendar Now
+          </h2>
+          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
+            It&lsquo;s free!
+          </p>
+        </a>
+      </div>
+
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <div className="left-0 top-0 flex w-full justify-center lg:static lg:w-auto">
-          <a
-            href="https://chrome.google.com/webstore/detail/ufc-calendar-with-live-up/pdagojbjdekpdicaefbiolkpgobiaoch"
-            className="group rounded-lg border px-5 py-4 transition-colors border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              src={"/chromestore.png"}
-              alt="logo"
-              width={340}
-              height={96}
-            />
-            <h2 className={`mb-3 text-2xl font-semibold`}>
-              Get UFC Calendar Now
-            </h2>
-            <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-              It&lsquo;s free!
-            </p>
-          </a>
-        </div>
         <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
           <a
             className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
