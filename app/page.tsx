ed90f135893d04d69afe625b0087fa22919c83fd
Issack John <issack.john1@ucalgary.ca>
2023-11-17 20:02:04 -0700

    nit

--- app/page.tsx
+++ app/page.tsx
@@ -57,7 +57,7 @@ export default function Home() {
           </a>
         </div>
       </div>
-      <h5>Available on Chrome & Edge Browsers</h5>
+      <p>Available on Chrome & Edge Browsers</p>
 
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
         <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
